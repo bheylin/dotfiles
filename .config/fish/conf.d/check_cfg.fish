2b4f5c5c7e467e748579d4b87990ba021d75380e
Brian Heylin <3947+bheylin@users.noreply.github.com>
2022-09-03 20:46:58 +0200

    Add unpushed commits check

--- .config/fish/conf.d/check_cfg.fish
+++ .config/fish/conf.d/check_cfg.fish
@@ -1,4 +1,11 @@
 if test $(cfg status --short | wc -l) -ne 0
-    echo "There are uncommited config changes tracked by the ~/.cfg repo"
-    echo "Commit them using: cfg commit -m 'message'"
+    echo "There are uncommitted config changes tracked by the ~/.cfg repo"
+    echo "Commit them using: cfg commit -am 'message'"
+    echo
+end
+
+if test $(cfg log @{u}.. | wc -l) -ne 0
+    echo "There are unpushed config changes tracked by the ~/.cfg repo"
+    echo "Push them using: cfg push"
+    echo 
 end
