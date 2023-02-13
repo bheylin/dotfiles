11d495863010205e07f656965dee6e2fd1020af6
Brian Heylin <3947+bheylin@users.noreply.github.com>
2023-02-13 15:29:19 +0100

    add atuin, rm fish fzf

--- .config/fish/conf.d/03_external_sourcing.fish
+++ .config/fish/conf.d/03_external_sourcing.fish
@@ -5,3 +5,9 @@ end
 if command -sq starship
     starship init fish | source
 end
+
+if command -sq atuin
+    set -gx ATUIN_NOBIND "true"
+    atuin init fish | source
+    bind \cr _atuin_search
+end
