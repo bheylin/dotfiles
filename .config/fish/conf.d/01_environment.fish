0f7e07510ad0d8ee72c579e33bfc5ecd17f40d6f
Brian Heylin <brianheylin@dreamsolution.nl>
2023-09-25 08:54:07 +0200

    Fish: set term as 'rxvt', set postgres vars

--- .config/fish/conf.d/01_environment.fish
+++ .config/fish/conf.d/01_environment.fish
@@ -1,9 +1,9 @@
 set fish_greeting ''
 set SHELL (which fish)
 
-set -gx TERMINAL "alacritty"
-set -gx EDITOR "hx"
 set -gx BROWSER /usr/bin/firefox
+set -gx EDITOR "hx"
+set -gx TERMINAL "rxvt"
 
 if command -sq rg
     set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden'
@@ -11,4 +11,6 @@ if command -sq rg
 end
 
 fish_add_path -U ~/.cargo/bin 
-fish_add_path -U /home/brianh/.local/share/gem/ruby/3.0.0/bin
+
+set -gx PGHOST /run/postgresql
+set -gx PGPORT 5432
