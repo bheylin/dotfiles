3b7ad8d660d7fd3a61c0811db41cb1fabd3ab47c
Brian Heylin <brianheylin@dreamsolution.nl>
2023-09-25 08:51:49 +0200

    Fish: Add clear to side-step i3 bug where prompt is positioned initially at offset from bottom

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,7 @@
 #
 # fisher install PatrickF1/fzf.fish
 # fisher install danhper/fish-ssh-agent
+
+# either i3 or xrvt is positioning the prompt at a const distance from
+# end of the window. Clearing the terminl buffer side-steps the issue
+clear
