fba74a9cb51341fe8c1e6e7f26bbfdac9b118c81
Brian Heylin <brianheylin@dreamsolution.nl>
2023-09-25 08:55:03 +0200

    Fish: ls alias uses eza, add cfg-ds alias

--- .config/fish/conf.d/02_aliases.fish
+++ .config/fish/conf.d/02_aliases.fish
@@ -1,6 +1,9 @@
-alias ls="exa"
+alias ls="eza"
 alias ll="ls -l"
 alias z="zoxide"
 
 # git repo that tracks personal dotfiles
 alias cfg='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+# git repo that tracks work dotfiles
+alias cfg-ds='git --git-dir=$HOME/.cfg-ds/ --work-tree=$HOME'
+alias gg='br --conf ~/.config/broot/git-diff-conf.toml --git-status'
